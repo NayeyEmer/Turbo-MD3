4c043bce550fde2291c0362c9f9036a5125d41c7
NayeyEmer <148182808+NayeyEmer@users.noreply.github.com>
2023-11-01 16:35:20 -0500

    Update store.js

--- store.js
+++ store.js
@@ -45,10 +45,10 @@ global.ig = 'https://instagram.com/jxtxn17'
 global.md = 'https://instagram.com/jxtxn17'
 global.paypal = 'https://instagram.com/jxtxn17'
 global.git = 'https://instagram.com/jxtxn17'
-global.nn = 'https://chat.whatsapp.com/LCAUbkf5kUz7jSxO6FADMU' //Grupo ofc
-global.nnn = 'https://chat.whatsapp.com/J1R402WH1N0Hdl3S0NDEYu' //Curiosity global
-global.nnnn = 'https://chat.whatsapp.com/KLpYFsitkgzJ15YQxZrBqO' //colaboración multi bots
-global.nnnnn = 'https://chat.whatsapp.com/KlRcEfl8snBEEOA0JXzw5A' //enlace curiosity 
+global.nn = 'https://chat.whatsapp.com/FGlXuc4B13G96PYdogNQDH' //Grupo ofc
+global.nnn = 'https://chat.whatsapp.com/FGlXuc4B13G96PYdogNQDH' //Curiosity global
+global.nnnn = 'https://chat.whatsapp.com/FGlXuc4B13G96PYdogNQDH' //colaboración multi bots
+global.nnnnn = 'https://chat.whatsapp.com/FGlXuc4B13G96PYdogNQDH' //enlace curiosity 
 global.linkSity = [yt, ig, md, paypal, git, nn]
 
 //Raíz
